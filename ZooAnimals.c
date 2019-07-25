f7b16cc11191dcea6cb0b6dc0b26b6b5e48a131a
stefano-u <stefanounlayao@Hotmail.ca>
2019-07-24 20:06:21 -0400

    New Info

--- ZooAnimals.c
+++ ZooAnimals.c
@@ -18,11 +18,13 @@ typedef struct animal {
 animal_t* createList(FILE*);
 animal_t* createNodeFromFile(FILE*);
 void displayList(animal_t*);
+void displayListBrief(animal_t*);
 void deleteList(animal_t**);
+void printToFile(FILE*, animal_t*);
 
 int main() {
     // Opens text file for reading/writing
-    FILE *fp = fopen("data.txt", "r+");
+    FILE *fp = fopen("dataNEW.txt", "r+");
     if (fp == NULL) {
         printf("Error reading file.\n");
         exit(1);
@@ -30,13 +32,43 @@ int main() {
 
     // Create linked list from file
     animal_t* list = createList(fp);
-    
+
+
+    puts("=== Welcome to the Zoo Gallery ===\n");
+    int choice;
+
+    do {
+        puts("Please select from one of the following options:");
+        puts("'1' to show all records");
+        puts("'2' to show details of a specific record");
+        puts("'3' to add a record");
+        puts("'5 to edit a record");
+        puts("'6' to delete a record");
+        puts("'7' to search for a record");
+        puts("'0' to quit");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 0:
+                break;
+            case 1:
+                displayListBrief(list);
+                break;
+            case 2:
+                break;
+
+        }
+
+    } while (choice != 0);
     // Display contents of the list
-    displayList(list);
-    
+    //    displayList(list);
+
     // Close file pointer
     fclose(fp);
-    
+
+    // Print to file
+    printToFile(fp, list);
+
     // Free linked list & contents of nodes
     deleteList(&list);
 
@@ -44,6 +76,7 @@ int main() {
 }
 
 // Creates a linked list based on data on file
+
 animal_t* createList(FILE *fp) {
     animal_t *node = NULL, *current = NULL, *head = NULL;
 
@@ -61,10 +94,11 @@ animal_t* createList(FILE *fp) {
 }
 
 // Create nodes based on input from file
+
 animal_t* createNodeFromFile(FILE* fp) {
-    
+
     char temp[MAX_LEN] = {0};
-    
+
     // Checks to see if there is more data in the file
     if (fgets(temp, sizeof (temp), fp) != NULL) {
 
@@ -74,17 +108,17 @@ animal_t* createNodeFromFile(FILE* fp) {
             printf("Cannot allocate memory for this animal!\n");
             return node;
         }
-        
+
         // Remove "\r\n" characters from the temporary string
         REMOVERN(temp);
-        
+
         node->next = NULL;
 
         // Used for breaking the string by the commas
         char* token;
 
         if (strlen(temp) > 0) {
-            
+
             // Animal ID            
             token = strtok(temp, ",");
             node->animalID = strtol(token, NULL, 10);
@@ -129,6 +163,7 @@ animal_t* createNodeFromFile(FILE* fp) {
 }
 
 // Display contents of linked list
+
 void displayList(animal_t* head) {
     if (head == NULL) {
         printf("Linked List is empty!\n");
@@ -141,7 +176,22 @@ void displayList(animal_t* head) {
     }
 }
 
+void displayListBrief(animal_t* head) {
+    if (head == NULL) {
+        printf("Linked List is empty!\n");
+    } else {
+        animal_t* current = head;
+        puts("\n");
+        while (current != NULL) {
+            printf("%d) %s (%c)\n", current->animalID, current->name, current->sex);
+            current = current->next;
+        }
+        puts("\n");
+    }
+}
+
 // Free linked list & contents of nodes
+
 void deleteList(animal_t** head) {
     animal_t* current = *head;
     animal_t* nextLocation = NULL;
@@ -151,5 +201,17 @@ void deleteList(animal_t** head) {
         free(current->location);
         free(current);
         current = nextLocation;
+    }
+}
+
+void printToFile(FILE* fp, animal_t* head) {\
+    if (head == NULL) {
+        printf("Linked List is empty! Cannot print to file.\n");
+    } else {
+        animal_t* current = head;
+        while (current != NULL) {
+            fprintf(fp, "%d,%s,%c,%d,%s\r\n", current->animalID, current->name, current->sex, current->quantity, current->location);
+            current = current->next;
+        }
     }
 }
